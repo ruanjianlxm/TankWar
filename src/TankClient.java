b67964fb80303e5fec29f894f72dc85f57dba3ce
刘小明 <657874721@qq.com>
2012-07-29 23:18:27 +0800

    初步完成红色坦克移动

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,39 +1,97 @@
-import java.awt.Color;
-import java.awt.Frame;
-import java.awt.Graphics;
+import java.awt.*;
+
 import java.awt.event.*;
 
-
-public class TankClient extends Frame{
  
+public class TankClient extends Frame{
+  public static final  int GAME_WIDTH = 800;
+  public static final  int GAME_HEIGHT = 600;
+  
+	  int x = 50 , y = 50;
+		Image offScreenImage =null;
 	public void paint(Graphics g) {
 	Color c = g.getColor();
 	g.setColor(Color.RED);
-	g.fillOval(50, 50, 30, 30);
+	g.fillOval(x, y, 30, 30);
 	g.setColor(c);
+
+	}
+	
+	public void update(Graphics g) {
+		if(offScreenImage == null) {
+			offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+		}
+		Graphics gOffScreen = offScreenImage.getGraphics();
+		Color c = gOffScreen.getColor();
+		gOffScreen.setColor(Color.GREEN);
+		gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+		gOffScreen.setColor(c);
+		paint(gOffScreen);
+		g.drawImage(offScreenImage, 0, 0, null);
 	}
 
-	public void launchFrame() {
+
+	public void lauchFrame() {
     	this.setLocation(400,300);
-    	this.setSize(800,600);
-    	this.setBackground(Color.GREEN);
-    	setVisible(true); 
+    	this.setSize(GAME_WIDTH,GAME_HEIGHT);
     	this.setTitle("TankWar");
 		this.addWindowListener(new WindowAdapter() {
-
-			@Override
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
 		});
 		this.setResizable(false);
+    	this.setBackground(Color.GREEN);
+		this.addKeyListener(new KeyMonitor());
+		setVisible(true); 
+		new Thread(new PaintThread()).start();
 		
 	}
 	
 	public static void main(String[] args) {
 		TankClient tc = new TankClient();
-		tc.launchFrame();
+		tc.lauchFrame();
+		
+	}
+	private class PaintThread implements Runnable {
+		
+		public void run() {
+		  while(true){
+			  repaint();
+			  try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		  }
+			
+		}
+		
 		
 	}
 
+	private class KeyMonitor extends KeyAdapter{
+		public void keyPressed(KeyEvent e) {
+			 int key = e.getKeyCode();
+		      switch (key) {
+			   case KeyEvent.VK_LEFT:
+				   x-=5;
+			       break;
+			   case KeyEvent.VK_UP:
+				   y-=5;
+			        break;
+			   case KeyEvent.VK_RIGHT:
+				   x+=5;
+			        break;
+			   case KeyEvent.VK_DOWN:
+				   y+=5;
+			   break;
+				
+			
+			}
+			 
+		}
+		
+	}
 }
